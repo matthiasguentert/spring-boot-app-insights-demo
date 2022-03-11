88d15f894a241b125c39e770f7ff2c9b3dfb363d
Matthias Güntert <matthias.guentert@gmail.com>
2022-03-11 22:22:12 +0100

    Initial commit

---
+++ src/main/java/io/azureblue/springbootappinsightsdemo/SpringBootAppInsightsDemoApplication.java
@@ -0,0 +1,13 @@
+package io.azureblue.springbootappinsightsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppInsightsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAppInsightsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/azureblue/springbootappinsightsdemo/SpringBootAppInsightsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.azureblue.springbootappinsightsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppInsightsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
